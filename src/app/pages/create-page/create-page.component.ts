1e362e369aa863a94e17a5873476c14bc97eddaa
AlexandruPopusoi <alexpop8827@gmail.com>
2024-11-29 09:23:43 +0200

    Added nr of questions to the modes pages. Formated some code.

--- src/app/pages/create-page/create-page.component.ts
+++ src/app/pages/create-page/create-page.component.ts
@@ -3,28 +3,28 @@ import { NavbarComponent } from '../../navbar/navbar.component';
 import { CollapsiblePanelComponent } from '../../create/collapsible-panel/collapsible-panel.component';
 import { QuizProcessorService } from '../../create/quiz-processor.service';
 import { RouterLink } from "@angular/router";
-import {MatButtonModule} from "@angular/material/button";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: 'app-create-page',
   standalone: true,
-    imports: [NavbarComponent, CollapsiblePanelComponent, RouterLink, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [NavbarComponent, CollapsiblePanelComponent, RouterLink, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './create-page.component.html',
   styleUrl: './create-page.component.css'
 })
 export class CreatePageComponent {
-  isHidden:boolean = true;
-  quizTitle='';
+  isHidden: boolean = true;
+  quizTitle = '';
 
-  constructor(protected quizProcessorService: QuizProcessorService){}
+  constructor(protected quizProcessorService: QuizProcessorService) { }
 
-  trigger(){
+  trigger() {
     this.isHidden = !this.isHidden;
   }
 
-  addTitle(event:any){
+  addTitle(event: any) {
     this.quizProcessorService.modifyQuizTitle(event.target.value);
   }
 }
--- src/app/pages/modes-page/modes-page.component.ts
+++ src/app/pages/modes-page/modes-page.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../navbar/navbar.component";
-import { CardComponent } from '../../card/card.component';
 import { RouterOutlet } from '@angular/router';
 import { QuizProcessorService } from '../../create/quiz-processor.service';
 
 @Component({
   selector: 'app-modes-page',
   standalone: true,
-  imports: [NavbarComponent, CardComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './modes-page.component.html',
   styleUrl: './modes-page.component.css'
 })
 export class ModesPageComponent {
 
-  constructor(protected quizProcessorService:QuizProcessorService){}
+  constructor(protected quizProcessorService: QuizProcessorService) { }
 
-  trigger(){
+  trigger() {
     console.log('yo');
   }
 
